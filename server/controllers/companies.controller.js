1ff59f38921cfb5f91b1934f59e64522c920bdfa
timocodex <timothy.og.siahaan@gmail.com>
2017-03-17 15:18:08 +0700

    done update profile

--- server/controllers/companies.controller.js
+++ server/controllers/companies.controller.js
@@ -1,16 +1,54 @@
 var Company = require('../models/model_company')
 var mongoose = require('mongoose');
+const multer = require('multer')
 
 module.exports={
   register: function(req,res){
     var newCompany = new Company({
-      email: req.body.email
+      email: req.body.email,
+      request:[],
+      verified:false,
+      created_at:new Date(),
+      updated_at:new Date()
     })
     newCompany.save(function(err){
       if(err) throw err
       res.json(newCompany)
     })
   },
+  editProfile: function(req,res){
+    Company.findOne({_id:req.params.id},function(err,company){
+      console.log(req.file);
+      if(err){
+        res.send(err)
+      }
+      else{
+        company.name = req.body.name
+        company.type = req.body.type
+        company.category = JSON.parse(req.body.category)
+        company.password = req.body.password
+        company.location.lat = req.body.lat
+        company.location.lng = req.body.lng
+        company.website = req.body.website
+        company.address = req.body.address
+        company.phone = req.body.phone
+        company.description = req.body.description
+        company.images = req.file
+        company.updated_at = new Date()
+        company.markModified('category')
+        company.save(function(err){
+          if(err){
+            res.send(err)
+          }
+          else{
+            res.json(company)
+          }
+        })
+
+      }
+    })
+  },
+
   showAll: function(req,res){
     Company.find().then(function(result){
       res.json(result)
--- server/routes/companies.js
+++ server/routes/companies.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var company = require('../controllers/companies.controller.js')
+const multer = require('multer')
 
 router.post('/auth/register',company.register)
+router.put('/:id',multer({ dest: './uploads/' }).single('uploads'),company.editProfile)
 router.get('/',company.showAll)
 router.delete('/',company.deleteAll)
 module.exports = router;
