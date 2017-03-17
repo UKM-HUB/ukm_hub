7759f12ec82f54bfb0917b679f24113566568369
timocodex <timothy.og.siahaan@gmail.com>
2017-03-17 10:04:43 +0700

    change the company model

--- server/models/model_company.js
+++ server/models/model_company.js
@@ -3,23 +3,27 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 // create a schema
-var userSchema = new Schema({
+var companySchema =  new Schema({
   name: String,
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  admin: Boolean,
-  location: String,
-  meta: {
-    age: Number,
-    website: String
+  email: { type: String, required: true, unique: true },
+  password: String,
+  category: Schema.Types.Mixed,
+  type: String,
+  location:{
+    lat:String,
+    lng:String
   },
+  request: Schema.Types.Mixed,
+  description: String,
+  website: String,
+  address: String,
   created_at: Date,
   updated_at: Date
 });
 
 // the schema is useless so far
 // we need to create a model using it
-var User = mongoose.model('User', userSchema);
+var Company = mongoose.model('Company', companySchema);
 
 // make this available to our users in our Node applications
-module.exports = User;
+module.exports = Company;
