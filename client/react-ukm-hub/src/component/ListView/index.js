bf9f48f73532c3ecfd23ea899e0ac4eac5017541
timocodex <timothy.og.siahaan@gmail.com>
2017-03-21 03:47:03 +0700

    reduxify list view done

--- client/react-ukm-hub/src/component/ListView/index.js
+++ client/react-ukm-hub/src/component/ListView/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-
+import {connect} from 'react-redux'
+import {fetchCompanyByCategory} from '../../actions/index.js'
 import Sidebar from '../Sidebar'
 import Topbar from '../Topbar'
 import CompanyList from './CompanyList'
+const compId = localStorage.getItem('companyId')
 
-export default class ListView extends Component {
+class ListView extends Component {
   constructor(){
     super()
     this.state = {
@@ -13,15 +15,39 @@ export default class ListView extends Component {
     }
   }
 
+  componentDidMount(){
+    this.props.fetchCompanyByCategory(compId)
+  }
   render () {
     return (
       <div className="wrapper">
         <Sidebar activeNavigation={this.state.activeNavigation} />
         <div className="main-panel">
           <Topbar title={this.state.topbarTitle} />
-          <CompanyList />
+            {
+              this.props.otherCompany.map((company) => {
+                return(
+                  <CompanyList key={company._id} {...company}/>
+                )
+              })
+            }
         </div>
       </div>
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    otherCompany: state.companyByCategory
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchCompanyByCategory: (id) => dispatch(fetchCompanyByCategory(id))
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(ListView)
