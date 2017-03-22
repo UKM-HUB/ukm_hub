74292f75aace40fdef5cba1b1590d2e7bdc143d4
timocodex <timothy.og.siahaan@gmail.com>
2017-03-22 21:06:17 +0700

    fix email style

--- server/controllers/companies.controller.js
+++ server/controllers/companies.controller.js
@@ -315,13 +315,16 @@ module.exports={
               else{
                 sendEmail(datas.email,
                    "You received a new message",
-                   `  <div style="text-align:center"><img src='https://raw.githubusercontent.com/UKM-HUB/ukm_hub/master/ukmhub.png' style="width:30%;"/></div>
-                      <P>Dear <b>${datas.name}</b>,</P>
-                      <p>to respond your request with subject <i><b>${datas.requestTitle}</b></i>, <b>${data.name}</b> have send you a message with title <b>"<i>${data.letter[data.letter.length-1].title}</i>"</b> .</p>
-                      <p>please check your message box immediately to accept or refused the offer, Thank you for your attention </p>
-                      <br/>
-                    <p>regards,</p>
-                      <p>UKM HUB teams</p> `,
+                   `    <div style="border:1px solid black;width:400px;margin:auto"><div style="text-align:center"><img src='https://raw.githubusercontent.com/UKM-HUB/ukm_hub/master/ukmhub.png' style="width:30%;"/></div>
+                        <div style="margin-left:20%;margin-right:20%">
+                        <P>Dear <b>${datas.name}</b>,</P>
+                        <p>to respond your request with subject <i><b>${req.body.requestTitle}</b></i>, <b>${data.name}</b> have send you a message with title <b>"<i>${data.letter[data.letter.length-1].title}</i>"</b> .</p>
+                        <p>please check your message box immediately to accept or refused the offer, Thank you for your attention </p>
+                        <br/>
+                      <p>regards,</p>
+                        <p>UKM HUB teams</p>
+                        </div>
+                        </div> `,
                     "Your received new message", res)
               }
             }
@@ -358,13 +361,16 @@ module.exports={
                               }
                               else{
                                 sendEmail(result2.email, "Accepted request",
-                                `  <div style="text-align:center"><img src='https://raw.githubusercontent.com/UKM-HUB/ukm_hub/master/ukmhub.png' style="width:40%;"/></div>
-                                  <P>Dear <b>${result2.name}</b>,</P>
-                                  <p><b>${result.name}</b> have <b>Accepted</b> your offer with title <b><i>"${sender.title}"</i></b> with message of "${sender.message}".</p>
-                                  <p>for the request of <i><b>${receptor.title}</b></i>, contact <b>${result.name}</b> for futher information.</p>
-                                  <br/>
-                                  <p>regards,</p>
-                                  <p>UKM HUB teams</p> `,
+                                `  <div style="border:1px solid black;width:400px;margin:auto"><div style="text-align:center"><img src='https://raw.githubusercontent.com/UKM-HUB/ukm_hub/master/ukmhub.png' style="width:30%;"/></div>
+                                    <div style="margin-left:20%;margin-right:20%">
+                                    <P>Dear <b>${result2.name}</b>,</P>
+                                    <p><b>${result.name}</b> have <b>Accepted</b> your offer with title <b><i>"${sender.title}"</i></b> with message of "${sender.message}".</p>
+                                    <p>for the request of <i><b>${receptor.requestTitle}</b></i>, contact <b>${result.name}</b> for futher information.</p>
+                                    <br/>
+                                    <p>regards,</p>
+                                    <p>UKM HUB teams</p>
+                                    </div>
+                                    </div> `,
                                   "Your request have been accepted"
                                   , res)
 
@@ -413,13 +419,16 @@ module.exports={
                               else{
                                 sendEmail(result2.email,
                                   "refused request",
-                                  `  <div style="text-align:center"><img src='https://raw.githubusercontent.com/UKM-HUB/ukm_hub/master/ukmhub.png' style="width:40%;"/></div>
+                                  `   <div style="border:1px solid black;width:400px;margin:auto"><div style="text-align:center"><img src='https://raw.githubusercontent.com/UKM-HUB/ukm_hub/master/ukmhub.png' style="width:30%;"/></div>
+                                    <div style="margin-left:20%;margin-right:20%">
                                     <P>Dear <b>${result2.name}</b>,</P>
                                     <p><b>${result.name}</b> have refused your offer with title <b><i>"${sender.title}"</i></b> with message of "${sender.message}".</p>
-                                    <p>for the request of <i><b>${receptor.title}</b></i>, contact <b>${result.name}</b> for futher information.</p>
+                                    <p>for the request of <i><b>${receptor.requestTitle}</b></i>, contact <b>${result.name}</b> for futher information.</p>
                                     <br/>
                                     <p>regards,</p>
-                                    <p>UKM HUB teams</p> `
+                                    <p>UKM HUB teams</p>
+                                    </div>
+                                    </div>`
                                    ,"Your request have been refused", res)
 
                               }
