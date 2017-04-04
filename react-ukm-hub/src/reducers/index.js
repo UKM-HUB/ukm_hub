60e9c30a1ae9b9e311d898cb713f6d0da2fe6b85
dgana <dhegana@gmail.com>
2017-04-04 23:08:38 +0700

    reducer update

--- react-ukm-hub/src/reducers/index.js
+++ react-ukm-hub/src/reducers/index.js
@@ -4,6 +4,7 @@ import profileReducer from './reducer_companyProfile.js'
 import companyByCategoryReducer from './reducer_company_by_category.js'
 import otherCompanyRequestReducer from './reducer_other_company_request.js'
 import searchKeyReducer from './reducer_search_key.js'
+
 const rootReducers = combineReducers({
   profile: profileReducer,
   loggedInCompany: loginReducer,
